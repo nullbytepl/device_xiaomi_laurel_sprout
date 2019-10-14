12d54a1ec5f0ece028701bb919a4f875b648b9f7
Jake Weinstein <jake@aospa.co>
2019-10-14 06:30:32 +0200

    laurel_sprout: add empty init script to set device properties
    Adapted from LineageOS
    
    Change-Id: I1775865dac3c3df53911e04329e982060bd02546
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: HarukeyUA <inazarrusnak@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -169,5 +169,9 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
+# Vendor init
+TARGET_INIT_VENDOR_LIB := libinit_laurel_sprout
+TARGET_RECOVERY_DEVICE_MODULES := libinit_laurel_sprout
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/laurel_sprout/BoardConfigVendor.mk
