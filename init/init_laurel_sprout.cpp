f1be21cdc1c915e281b17fcbe0f694acbad050a7
DennySPB <dennyspb@gmail.com>
2019-12-20 16:19:52 +0300

    init: override ro.control_privapp_permissions
    set to log only for mitigation open gapps permissions issues
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Ief0693594f42334c46063e6028cedf86a392269e

--- init/init_laurel_sprout.cpp
+++ init/init_laurel_sprout.cpp
@@ -58,4 +58,5 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
 void vendor_load_properties()
 {
 	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+	property_override_dual("ro.control_privapp_permissions", "ro.control_privapp_permissions", "log");
 }
