86d4ae1ed2bad8bc6715111ba43fd98a15af08ab
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-04-05 08:28:35 +0000

    laurel_sprout: Update fingerprint to coral march

--- init/init_laurel_sprout.cpp
+++ init/init_laurel_sprout.cpp
@@ -57,7 +57,7 @@ void property_override_dual(char const system_prop[], char const vendor_prop[], 
 
 void vendor_load_properties()
 {
-	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ2A.200305.003/6156912:user/release-keys");
 	property_override_dual("ro.control_privapp_permissions", "ro.control_privapp_permissions", "log");
 	property_override("ro.oem_unlock_supported", "0");
 
