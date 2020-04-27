f9ac3a8e1f6a8ef190292ff02ec13890e9625ba9
Kuba Wojciechowski <nullbytepl@gmail.com>
2020-04-27 10:42:39 +0000

    laurel_sprout: sepolicy: remove permissive domains

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,15 +151,15 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/temp
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-#    device/qcom/sepolicy/generic/private \
-#    device/qcom/sepolicy/qva/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
 
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-#    device/qcom/sepolicy/generic/public \
-#    device/qcom/sepolicy/qva/public
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
 
 # Treble
 BOARD_VNDK_VERSION := current
